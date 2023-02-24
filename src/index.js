a40f222c57a023619f88d7051f91bebe0088b9a5
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-02-25 00:49:41 +0530

    react proj

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Import React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//Get a reference to the div with ID root from index.html
+const el = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Tell React to take control of above element
+const root = ReactDOM.createRoot(el);
+
+//create a component
+function App() {
+  //This is not a HTML element but a JSX element
+  return <h1>Hello! You got this.</h1>;
+}
+
+//Show the component on the screen
+root.render(<App />);

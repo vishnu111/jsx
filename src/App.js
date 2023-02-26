74ff87dc7dbf6bc97d1a1042a6add9e04f1ed700
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-02-27 01:38:19 +0530

    react animals proj

---
+++ src/App.js
@@ -0,0 +1,6 @@
+function App() {
+  //This is not a HTML element but a JSX element
+  return <h1>Hello! You got this.</h1>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 //Import React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./App";
 //Get a reference to the div with ID root from index.html
 const el = document.getElementById("root");
 
 //Tell React to take control of above element
 const root = ReactDOM.createRoot(el);
-
-//create a component
-function App() {
-  //This is not a HTML element but a JSX element
-  return <h1>Hello! You got this.</h1>;
-}
 
 //Show the component on the screen
 root.render(<App />);
